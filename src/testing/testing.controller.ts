052aa1a49ba272d52da6a64ea6153684b597587a
Mikhail Smirnov <smirnov.mic@yandex.ru>
2023-02-15 15:05:03 +0300

    1

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -27,6 +27,8 @@ END;
 $$ LANGUAGE plpgsql;
 SELECT truncate_tables('postgres');
 SELECT truncate_tables('fqidihfj');
+SELECT truncate_tables('qTheSky'); 
+SELECT truncate_tables('neondb'); 
         `);
       return;
     } catch (e) {
