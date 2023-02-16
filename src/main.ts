4af63e2d9700a167002c319ed8e0033221ba002b
Mikhail Smirnov <smirnov.mic@yandex.ru>
2023-02-16 14:18:55 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
   });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalInterceptors(new TrimStrings());
