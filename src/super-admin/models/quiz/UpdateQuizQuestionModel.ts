22b6f5230439b740b6f6fb73da7cc0d7a7088d04
Mikhail Smirnov <smirnov.mic@yandex.ru>
2023-02-18 15:41:05 +0300

    fix length question

--- src/super-admin/models/quiz/UpdateQuizQuestionModel.ts
+++ src/super-admin/models/quiz/UpdateQuizQuestionModel.ts
@@ -1,7 +1,8 @@
-import { IsArray, IsString } from 'class-validator';
+import { IsArray, IsString, Length } from 'class-validator';
 
 export class UpdateQuizQuestionModel {
   @IsString()
+  @Length(0, 1000)
   body: string;
   @IsArray()
   @IsString({ each: true })
