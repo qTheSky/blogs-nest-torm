5f3ebf08246b276bc3c50e071eaa9bc0aba22f23
Mikhail Smirnov <smirnov.mic@yandex.ru>
2023-02-24 19:47:40 +0300

    1

--- src/common/pipes/parse-number-pipe.ts
+++ src/common/pipes/parse-number-pipe.ts
@@ -1,12 +1,12 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, Injectable, NotFoundException, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class ParseNumberPipe implements PipeTransform<string, number | string> {
   transform(value: string, metadata: ArgumentMetadata) {
     const parsed = Number(value);
     if (typeof parsed !== 'number' || isNaN(parsed)) {
-      // throw new NotFoundException();
-      return value;
+      throw new NotFoundException('Id should be string which can be converted to number');
+      // return value;
     }
     return parsed;
   }
